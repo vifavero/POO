f73c6d89336daa578ec62d9bbf9aa7e7c31c5360
Victoria Fávero <110290761+vifavero@users.noreply.github.com>
2025-03-20 09:24:28 -0300

    Add files via upload
    🎉 Commit inicial

---
+++ mavenproject1/src/main/java/br/executordeclasses/Pessoa.java
@@ -0,0 +1,20 @@
+package br.executordeclasses;
+
+public class Pessoa {
+    public String identificaSe() {
+        return "Sou uma pessoa.";
+    }
+
+    public String respondePergunta() {
+        return "Depende...";
+    }
+
+    public double computa(double num1, double num2) {
+        return (num1+num2);
+    }    
+    
+    public String despedeSe() {
+        return "Até mais!";
+    }
+}
+
---
+++ mavenproject1/src/main/java/br/executordeclasses/aluno_15698302.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.executordeclasses;
+
+/**
+ *
+ * @author 15698302
+ */
+public class aluno_15698302 extends Pessoa{
+    public String identificaSe() {
+        return "Sou aluno USP";
+    }
+
+    public String respondePergunta() {
+        return "Depende do contexto";
+    }
+
+    public double computa(double num1, double num2) {
+        return (num1*num2);
+    }    
+    
+    public String despedeSe() {
+        return "Até mais!";
+    }
+
+}
